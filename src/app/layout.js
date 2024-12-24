f21072ba56fb0a394588b640c99afc38d903ba98
Riki Rusdian <rikirusdian@gmail.com>
2024-12-24 08:54:02 +0700

    add gtags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,30 @@
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
+/*
 export default function RootLayout({ children }) {
   return children
+}
+*/
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Script GA4 - gtag.js */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0C6THVCTKG" strategy="afterInteractive"></script>
+        <script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0C6THVCTKG');
+        `}
+        </script>
+        </head>
+        <body>
+        {children}
+      </body>
+    </html>
+  )
 }
