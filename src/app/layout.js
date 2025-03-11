3b399f542e4bd6fee587512204e6421050b67381
ricky alamsyah permana <rickyalamsyahp@gmail.com>
2025-03-11 20:16:26 +0700

    fixing

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,20 +8,7 @@ export default function RootLayout({ children }) {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        {/* Script GA4 - gtag.js */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0C6THVCTKG" strategy="afterInteractive"></script>
-        <script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-0C6THVCTKG');
-        `}
-        </script>
-        </head>
+    <html lang="id">
         <body>
         {children}
       </body>
